9132b105768ad7f68a4de118c35b01ba0627899f
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-09-27 20:40:06 -0700

    we tried to pass out test but got stuck. Will need to change our approach to the prototype. Co-authored by: Hans Ellis <bullhorn@gmail.com> Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -8,4 +8,20 @@ describe('Board', ()=> {
     expect(sudokuBoard.columns).toEqual(9);
     expect(sudokuBoard.boxes).toEqual(9);
   });
+
+  test('should check if each cell is empty in a rows and columns', () => {
+    const testBoard = [
+      ["2","3",".","9","4",".","6","7","."],
+      ["8",".",".","3","2","5","9","1","4"],
+      ["9",".",".","7","6",".","3","2","."],
+      ["1",".",".",".",".",".","7","9","2"],
+      ["5",".","3","2","1",".","4","8","6"],
+      ["4",".",".","6","8",".","5","3","1"],
+      ["7",".",".","1",".",".",".",".","9"],
+      ["6","5","9","8","7","2","1","4","3"],
+      ["3",".",".",".","9",".",".",".","7"]
+    ];
+    const sudokuBoard = new Board(testBoard);
+    expect(sudokuBoard.checkSudokuValid()).toEqual(testBoard);
+  });
 });
