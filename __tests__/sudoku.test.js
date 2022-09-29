b9ffc084ed3a8cb18cad72ebc52dc1b37a11ca63
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-09-28 20:39:47 -0700

    Are still trying to get third test to pass. Co-authored-by: Peter Philavanh <efl2ice89@gmail.com> Co-authored by: Hans Ellis <bullhorn@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -3,9 +3,9 @@ import Board from './../src/sudoku.js';
 describe('Board', ()=> {
 
   test('should create a board object with three properties', () => {
-    const sudokuBoard = new Board(9);
-    expect(sudokuBoard.rows).toEqual(9);
-    //expect(sudokuBoard.columns).toEqual(9);
+    const sudokuBoard = new Board(this.rows,this.columns);
+    expect(sudokuBoard.rows).toEqual(sudokuBoard.rows);
+    expect(sudokuBoard.columns).toEqual(sudokuBoard.columns);
     //expect(sudokuBoard.boxes).toEqual(9);
   });
 
@@ -13,7 +13,24 @@ describe('Board', ()=> {
     const testBoard = [
       ["2","3",".","9","4",".","6","7","."]
     ];
-    const sudokuBoard = new Board(testBoard);
+    const sudokuBoard = new Board(testBoard,[]);
     expect(sudokuBoard.checkSudokuValid()).toEqual(testBoard);
   });
+
+  test('should check if each cell is empty in a row and column', () => {
+    const testBoard = [
+      ["2","3",".","9","4",".","6","7","."]
+      ["8",".",".","3","2","5","9","1","4"],
+      ["9",".",".","7","6",".","3","2","."],
+      ["1",".",".",".",".",".","7","9","2"],
+      ["5",".","3","2","1",".","4","8","6"],
+      ["4",".",".","6","8",".","5","3","1"],
+      ["7",".",".","1",".",".",".",".","9"],
+      ["6","5","9","8","7","2","1","4","3"],
+      ["3",".",".",".","9",".",".",".","7"]
+    ];
+    const sudokuBoard = new Board(testBoard,testBoard);
+    expect(sudokuBoard.checkSudokuValid()).toEqual(testBoard);
+  })
 });
+
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,30 +1,34 @@
-export default function Board(rowsArr = [])  {
+export default function Board(rowsArr, columns)  {
   this.rows = rowsArr;
-  //this.columns = [];
+  this.columns = columns;
   //this.boxes = [[]];
 }
 
 const testBoard = [
   ["2","3",".","9","4",".","6","7","."]
-/*  ["8",".",".","3","2","5","9","1","4"],
+  ["8",".",".","3","2","5","9","1","4"],
   ["9",".",".","7","6",".","3","2","."],
   ["1",".",".",".",".",".","7","9","2"],
   ["5",".","3","2","1",".","4","8","6"],
   ["4",".",".","6","8",".","5","3","1"],
   ["7",".",".","1",".",".",".",".","9"],
   ["6","5","9","8","7","2","1","4","3"],
-  ["3",".",".",".","9",".",".",".","7"]*/
+  ["3",".",".",".","9",".",".",".","7"]
 ];
 
 Board.prototype.checkSudokuValid = function() {
+  let row = this.rows;
+  let column = this.column;
   for (let i = 0; i < testBoard.length; i++) {
-    let cell = testBoard[i];
-
-    if (cell !== ".") {
+    for (let j = 0; j < testBoard.length; j++) {
+      let cell = testBoard[i][j];
       console.log(cell);
-      this.rows[i].push(cell);
-      //this.columns[j].push(cell);
+
+      if (cell !== ".") {
+        console.log(cell);
+        row[i].push(cell);
+        column[j].push(cell);
+      }
     }
   }
-  return this.rows;
 }
